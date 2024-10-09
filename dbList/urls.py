cde8b03bece2c12910cf8c1fb8f4c341c81d281d
안용범 <ahn@an-yongbeom-ui-MacBookPro.local>
2024-10-09 16:52:44 +0900

    remove dbList

--- dbList/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import ConnectionListView, ConnectionCreateView, ConnectionUpdateView
-
-urlpatterns = [
-    path('connections/', ConnectionListView.as_view(), name='connection_list'),
-    path('connections/create/', ConnectionCreateView.as_view(), name='connection_create'),
-    path('connections/<int:pk>/update/', ConnectionUpdateView.as_view(), name='connection_update'),
-]
